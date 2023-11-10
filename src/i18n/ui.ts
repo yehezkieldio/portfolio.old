d24d7465e5a9be43f8a43a11d81068c95b56a10e
elizielx <yehezkieldio35@gmail.com>
2023-11-10 08:14:36 +0800

    feat: some changes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,9 +7,13 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
-    "header.h1": "Hi-ya! I'm Yehezkiel Dio, a Software Developer.",
+    "header.h1": "Hi! I'm Yehezkiel Dio, a Software Developer.",
+    "header.h2":
+      "A passionate software developer based in Indonesia, specializing in full-stack web development with a strong interest in open-source development.",
   },
   id: {
     "header.h1": "Hai! Saya adalah Yehezkiel Dio, Software Developer.",
+    "header.h2":
+      "Pengembang perangkat lunak penuh semangat yang berbasis di Indonesia, berspesialisasi dalam pengembangan web full-stack dengan minat kuat pada pengembangan sumber terbuka.",
   },
 } as const;
